f474223014e1d4f539c214c7fe41aedde6a8f6f4
Nuray Altuğ <56605130+nurayaaltug@users.noreply.github.com>
2023-04-19 12:58:00 +0300

    Update exception.java

--- exception.java
+++ exception.java
@@ -6,6 +6,20 @@ If your code successfully converts  into a string  the code will print "Good job
 
 n can range between  to  inclusive.
  ***********************************************************************************************************************/
-if(n < -100 || n > 100) throw new DoNotTerminate.ExitTrappedException();
+import java.util.*;
+import java.security.*;
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   //Write your code here
+    if(n < -100 || n > 100) throw new DoNotTerminate.ExitTrappedException();
 
         String s = String.valueOf(n);
